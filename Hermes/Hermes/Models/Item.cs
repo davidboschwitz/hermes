085e5537af5ba6db846934d1021ce1afcc030267
Justin Kaufer <jtkaufer@iastate.edu>
2018-11-14 11:46:23 -0600

    update

--- Hermes/Hermes/Models/Item.cs
+++ Hermes/Hermes/Models/Item.cs
@@ -4,6 +4,13 @@ namespace Hermes.Models
 {
     public class Item
     {
+        public Item(string id, string text, string description)
+        {
+            Id = id;
+            Text = text;
+            Description = description;
+        }
+
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
