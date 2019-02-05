0d48772dcc81bfbe6d7e8801e8f88a37ff303af3
Caleb Nash <cjnash@iastate.edu>
2019-02-05 13:02:08 -0600

    Minimum Viable Product - Maps

---
+++ testing/GMAP1/GMAP1/GMAP1/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GMAP1
+{
+    public class App : Application // superclass new in 1.3
+    {
+        public App()
+        {
+
+            var tabs = new TabbedPage();
+
+            // demonstrates the map control with zooming and map-types
+            tabs.Children.Add(new MapPage { Title = "Map/Zoom", Icon = "glyphish_74_location.png" });
+
+            // demonstrates the map control with zooming and map-types
+            tabs.Children.Add(new PinPage { Title = "Pins", Icon = "glyphish_07_map_marker.png" });
+
+            MainPage = tabs;
+        }
+    }
+}
---
+++ testing/GMAP1/GMAP1/GMAP1/MapPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace GMAP1
+{
+    public class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(
+                        new Position(42.025250, -93.650870), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+
+            var slider = new Slider(1, 18, 1);
+            slider.ValueChanged += (sender, e) =>
+            {
+                var zoomLevel = e.NewValue; // between 1 and 18
+                var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+                map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+            };
+        }
+    }
+}
