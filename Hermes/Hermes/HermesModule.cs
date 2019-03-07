a7115b95bef800c255ca8acc69de94b1de0ce9ce
David Boschwitz <dbosch@iastate.edu>
2019-03-07 14:39:04 -0600

    Update dependency injection for Database and main app

---
+++ Hermes/Hermes/HermesModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Hermes.Database;
+using Hermes.Views;
+
+namespace Hermes
+{
+    public class HermesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new DatabaseModule());
+
+            builder.Register(c => new MainPage())
+                   .As<MainPage>()
+                   .SingleInstance();
+        }
+    }
+}
