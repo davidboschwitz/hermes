25d110b504ee4d9cff2c920c8c1859dd2764e379
David Boschwitz <dbosch@iastate.edu>
2019-03-07 18:05:06 -0600

    Set up Menu Bar

--- Hermes/Hermes/Views/MainPage.xaml.cs
+++ Hermes/Hermes/Views/MainPage.xaml.cs
@@ -1,16 +1,35 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using Hermes.Menu;
 using Xamarin.Forms.Xaml;
 
 namespace Hermes.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : TabbedPage
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
     {
-        public MainPage()
+
+        public MainPage(MenuPage menu)
         {
             InitializeComponent();
+
+            Master = menu;
+            Detail = menu.MenuItems[0].NavigationPage;
+            MasterBehavior = MasterBehavior.Popover;
+        }
+        
+        public async Task NavigateFromMenu(NavigationPage selectedPage)
+        {
+            if (selectedPage != null && Detail != selectedPage)
+            {
+                Detail = selectedPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
         }
     }
 }
