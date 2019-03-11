0eec2b4e375d1e05eb6686e828fbc06683272b54
David Boschwitz <dbosch@iastate.edu>
2019-03-11 18:06:25 -0500

    Move MenuPage into ViewsModule

--- Hermes/Hermes/Views/ViewsModule.cs
+++ Hermes/Hermes/Views/ViewsModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using Hermes.Menu;
+using System.Collections.Generic;
 
 namespace Hermes.Views
 {
@@ -6,9 +8,17 @@ namespace Hermes.Views
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.Register(c => new MenuPage(menuItems: c.Resolve<IEnumerable<HermesMenuItem>>()))
+                   .As<MenuPage>()
+                   .SingleInstance();
+
             builder.Register(c => new AboutPage())
-                .As<AboutPage>()
-                .SingleInstance();
+                   .As<AboutPage>()
+                   .SingleInstance();
+
+            builder.Register(c => new MainPage(c.Resolve<MenuPage>()))
+                   .As<MainPage>()
+                   .SingleInstance();
         }
     }
 }
