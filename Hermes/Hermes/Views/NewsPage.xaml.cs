b3a0b4f816f38e5e6fb897b15734b936567afad2
lfladung <loganfladung@gmail.com>
2019-03-12 13:20:10 -0500

    Start geolocation tracking

--- Hermes/Hermes/Views/NewsPage.xaml.cs
+++ Hermes/Hermes/Views/NewsPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Collections;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Plugin.Geolocator;
 using Hermes.Models;
 
 namespace Hermes.Views
@@ -15,14 +17,27 @@ namespace Hermes.Views
     public partial class NewsPage : ContentPage
     {
         private const string newLine = "\r\n";
+        private double latitude;
+        private double longitude;
+        private SearchBar searchBar;
 
         public NewsPage()
         {
             InitializeComponent();
             BindingContext = this;
-
-            this.Content = BuildView(); 
+            this.Content = BuildView();
         }
+
+        //void GetLastLocationFromDevice()
+        //{
+        //    var location = CrossGeolocator.Current;
+        //    var position = location.GetPositionAsync();
+
+        //    latitude = position.Result.Latitude;
+        //    longitude = position.Result.Longitude;
+        //    System.Diagnostics.Debug.WriteLine("Latitude {0}, Longitude {1}", this.latitude, this.longitude);
+
+        //}
 
 
         public List<Label> GenerateReports()
@@ -41,7 +56,7 @@ namespace Hermes.Views
                 Text = "Overnight rainfall of 3 to 5 inches across Story County flooded numerous roadways and triggered mudslides, with authorities responding to reports of people trapped in stranded vehicles and forecasters issuing an elevated flood warning for the region Wednesday morning.",
                 LocationBased = false,
                 LatLong = null
-               
+
             };
             ReportItem a2 = new ReportItem
             {
@@ -51,7 +66,7 @@ namespace Hermes.Views
                 Title = "Red Cross Giving Aid at Memorial Union",
                 Text = "The Red Cross is providing shelter, food, health services and emotional support during this challenging situation to those affected, like Rakiea, Jenna and Ollie, whose stories you can read here.  The Red Cross is working around the clock with our partners to get help to where it’s most needed, and we’re reaching more neighborhoods each day.",
                 LocationBased = true,
-                LatLong = new Tuple<Double, Double>( 42.023267, -93.645772 )
+                LatLong = new Tuple<Double, Double>(42.023267, -93.645772)
             };
             reportList.Add(a1);
             reportList.Add(a2);
@@ -64,13 +79,13 @@ namespace Hermes.Views
                     Text = reportList[i].From + newLine + reportList[i].TimeStamp + newLine + reportList[i].Title + newLine + reportList[i].Text + newLine,
                     BackgroundColor = Color.MistyRose
                 });
-                if(reportList[i].LocationBased == true)
+                if (reportList[i].LocationBased == true)
                 {
                     labelArr[i].Text += newLine + "Check Coordinates:" + reportList[i].LatLong + newLine;
                     labelArr[i].BackgroundColor = Color.NavajoWhite;
                 }
             }
-            return labelArr;
+        return labelArr;
         }
 
         public Grid BuildView()
@@ -79,6 +94,21 @@ namespace Hermes.Views
             Grid grid = new Grid { Padding = 20 };
             ScrollView scroll = new ScrollView { };
             StackLayout stack = new StackLayout { Orientation = StackOrientation.Vertical };
+            Label resultsLabel = new Label
+            {
+                Text = "Result will appear here.",
+                VerticalOptions = LayoutOptions.Fill,
+                FontSize = 14
+            };
+            searchBar = new SearchBar()
+            {
+                Placeholder = "Search here",
+                SearchCommand = new Command(() => { resultsLabel.Text = "Result: " + searchBar.Text + " is what you asked for."; })
+            };
+     
+
+            stack.Children.Add(resultsLabel);
+            stack.Children.Add(searchBar);
 
             grid.Children.Add(scroll);
             scroll.Content = stack;
@@ -86,7 +116,7 @@ namespace Hermes.Views
             {
                 stack.Children.Add(labelArr[i]);
                 //Terrible way to check if need button fix later
-                if(labelArr[i].BackgroundColor == Color.NavajoWhite)
+                if (labelArr[i].BackgroundColor == Color.NavajoWhite)
                 {
                     stack.Children.Add(new Button()
                     {
@@ -96,6 +126,12 @@ namespace Hermes.Views
             }
 
             return grid;
+        }
+
+        public Boolean CheckIfInRange(Tuple<Double, Double> reportLatLong)
+        {
+
+            return false;
         }
 
         public List<ReportItem> RetrieveData()
