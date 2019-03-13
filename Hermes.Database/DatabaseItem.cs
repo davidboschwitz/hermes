69da44f6fb4feb88a224a7bb1e6028cda96727ef
David Boschwitz <dbosch@iastate.edu>
2019-03-13 17:18:42 -0500

    Updates to DatabaseItem structure + unit tests

--- Hermes.Database/DatabaseItem.cs
+++ Hermes.Database/DatabaseItem.cs
@@ -4,14 +4,35 @@ namespace Hermes.Database
 {
     public abstract class DatabaseItem
     {
-        public Guid MessageID;
-        public Guid SenderID;
-        public Guid RecipientID;
+        public Guid MessageID { get; set; }
+        public Guid SenderID { get; set; }
+        public Guid RecipientID { get; set; }
 
-        public DateTime CreatedTimestamp;
-        public DateTime UpdatedTimestamp;
+        public DateTime CreatedTimestamp { get; set; }
+        public DateTime UpdatedTimestamp { get; set; }
 
-        public string MessageNamespace;
-        public string MessageName;
+        public string MessageNamespace { get; set; }
+        public string MessageName { get; set; }
+
+        public DatabaseItem()
+        {
+            // for sqlite autogeneration
+        }
+
+        public DatabaseItem(Guid messageID, Guid senderID, Guid recipientID, DateTime createdTimestamp, DateTime updatedTimestamp, string messageNamespace, string messageName)
+        {
+            MessageID = messageID;
+            SenderID = senderID;
+            RecipientID = recipientID;
+            CreatedTimestamp = createdTimestamp;
+            UpdatedTimestamp = updatedTimestamp;
+            MessageNamespace = messageNamespace;
+            MessageName = messageName;
+        }
+
+        public override string ToString()
+        {
+            return $"[{MessageID}]({SenderID}->{RecipientID})@{CreatedTimestamp}/{UpdatedTimestamp}<{MessageNamespace}.{MessageName}>";
+        }
     }
 }
---
+++ UITest/DatabaseTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using Hermes.Database;
+using NUnit.Framework;
+using SQLite;
+
+namespace UnitTests
+{
+    public class TestItem : DatabaseItem
+    {
+        public string Body { get; set; }
+
+        public TestItem() { }
+
+        public TestItem(string body) : base(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), DateTime.Now, DateTime.Now, "Test", "TestItem")
+        {
+            Body = body;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} {Body}";
+        }
+    }
+    public class TestItem2 : DatabaseItem
+    {
+        public int Body { get; set; }
+
+        public TestItem2() { }
+
+        public TestItem2(int body) : base(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), DateTime.Now, DateTime.Now, "Test", "TestItem")
+        {
+            Body = body;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} {Body}";
+        }
+    }
+
+    [TestFixture]
+    public class DatabaseTests
+    {
+        DatabaseController DatabaseController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine("SetUp:start");
+            DatabaseController = new DatabaseController();
+            TestContext.WriteLine("SetUp:end");
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("asdfasdf");
+            DatabaseController.CreateTable<TestItem>();
+            var db1 = new TestItem("bod1");
+            var db2 = new TestItem2(10);
+            DatabaseController.CreateTable<TestItem>();
+            TestContext.WriteLine(DatabaseController.Insert(db1));
+            TestContext.WriteLine(DatabaseController.Insert(db2));
+            foreach (var i in DatabaseController.Table<TestItem>())
+            {
+                TestContext.WriteLine(i);
+            }
+            foreach (var i in DatabaseController.Table<TestItem2>())
+            {
+                TestContext.WriteLine(i);
+            }
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            TestContext.WriteLine("asdfasdf");
+            DatabaseController.CreateTable<TestItem>();
+            DatabaseController.CreateTable<TestItem2>();
+            var db1 = new TestItem("bod1");
+            var db2 = new TestItem("bod2");
+            DatabaseController.CreateTable<TestItem>();
+            TestContext.WriteLine(DatabaseController.Insert(db1));
+            TestContext.WriteLine(DatabaseController.Insert(db2));
+            foreach (var i in DatabaseController.Table<TestItem>())
+            {
+                TestContext.WriteLine(i);
+            }
+            Assert.Pass();
+        }
+    }
+}
