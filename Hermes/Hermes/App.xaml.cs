0fd70ce58659f9810a7ec91ae2b886a9ed10f1dd
David Boschwitz <dbosch@iastate.edu>
2019-04-02 13:59:27 -0500

    Create platform specific helpers and HermesSupportService (helper methods for device info and storage)

--- Hermes/Hermes/App.xaml.cs
+++ Hermes/Hermes/App.xaml.cs
@@ -3,26 +3,31 @@ using Xamarin.Forms.Xaml;
 using Hermes.Views;
 using Autofac;
 using Hermes.Networking;
+using System;
+using System.Diagnostics;
+using Hermes.Services;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Hermes
 {
     public partial class App : Application
     {
-
         IContainer Container;
         INetworkController NetworkController;
 
-        public App()
+        public App() : this(new ContainerBuilder()) { }
+
+        public App(ContainerBuilder builder)
         {
             InitializeComponent();
-
-            var builder = new ContainerBuilder();
+            
             builder.RegisterModule(new HermesModule());
             Container = builder.Build();
 
-            var networkController = Container.Resolve<NetworkControllerFactory>().NetworkController;
+            var networkController = Container.Resolve<INetworkController>();
             NetworkController = networkController;
+
+            Container.Resolve<IHermesSupportService>().HermesIdentifier();
 
             var mainPage = Container.Resolve<MainPage>();
             MainPage = mainPage;
---
+++ Hermes/Hermes/Services/IHermesSupportService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hermes.Services
+{
+    public interface IHermesSupportService
+    {
+        string GetUniqueIdentifier();
+        Guid HermesIdentifier();
+    }
+}
