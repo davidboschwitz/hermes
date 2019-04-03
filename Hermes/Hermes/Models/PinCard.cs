68712f994a879ecbbf654e27b22296c8d0ae8fbf
Justin Kaufer <jtkauf16@yahoo.com>
2019-04-03 16:34:22 -0500

    Scrolling pin card feature complete

---
+++ Hermes/Hermes/Models/PinCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hermes.Models
+{
+    public class PinCard
+    {
+        public string Address { get; set; }
+        public string Info { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Hermes/Hermes/Pages/PinCardMap.cs
@@ -0,0 +1,48 @@
+﻿using Hermes.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Hermes.Pages
+{
+    class PinCardMap : ContentPage
+    {
+        public PinCardMap(Position p)
+        {
+            Position center = p;
+
+            OriginalCustomMap customMap = new OriginalCustomMap()
+            {
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                MapType = MapType.Street,
+            };
+
+            Button back = new Button
+            {
+                Text = "back"
+            };
+
+            back.Clicked += back_clickedAsync;
+
+            async void back_clickedAsync(object sender, EventArgs e)
+            {
+                await Navigation.PopModalAsync();
+            }
+
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(center.Latitude, center.Longitude), Distance.FromMiles(1.0)));
+
+            Content = new StackLayout
+            {
+                Spacing = 0,
+                Children = {
+                    back,
+                    customMap
+                }
+            };
+        }
+    }
+}
