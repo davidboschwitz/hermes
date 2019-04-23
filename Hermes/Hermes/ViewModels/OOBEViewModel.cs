89909b0b303d91ec7fdc5f8b5aaf20a01b577a40
David Boschwitz <dbosch@iastate.edu>
2019-04-23 14:41:51 -0500

    Begin OOBE page

---
+++ Hermes/Hermes/ViewModels/OOBEViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Hermes.Capability.Chat.Model;
+using Hermes.Database;
+using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Hermes.ViewModels
+{
+    public class OOBEViewModel : BaseViewModel
+    {
+        public string OOBEInputName { get; set; }
+        public ICommand CreateContactCommand { get; }
+        DatabaseController DatabaseController { get; }
+        NavigationPage NavigationPage { get; }
+
+        public OOBEViewModel(DatabaseController databaseController, NavigationPage navigationPage)
+        {
+            DatabaseController = databaseController;
+
+            CreateContactCommand = new Command(CreateContactFunction);
+        }
+
+        private void CreateContactFunction(object obj)
+        {
+            try
+            {
+                DatabaseController.CreateTable<ChatContact>();
+                var contact = new ChatContact(DatabaseController.PersonalID(), OOBEInputName);
+                DatabaseController.Insert(contact);
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("POOP1");
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine(e.StackTrace);
+                Debug.WriteLine("");
+            }
+            try { 
+            DatabaseController.SetProperty("OOBE", "done");
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("POOP2");
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine(e.StackTrace);
+                Debug.WriteLine("");
+            }
+            RootPage.SetNavigationRoot(NavigationPage);
+        }
+    }
+}
---
+++ Hermes/Hermes/Views/OOBEPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Hermes.Database;
+using Hermes.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hermes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OOBEPage : ContentPage
+    {
+
+        public OOBEPage(DatabaseController databaseController, NavigationPage navigationPage)
+        {
+            InitializeComponent();
+
+            BindingContext = new OOBEViewModel(databaseController, navigationPage);
+        }
+    }
+}
