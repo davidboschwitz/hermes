fb1ba2dfb3c5285167aa87a389fd523ff039aa70
Caleb Nash <cjnash@iastate.edu>
2019-04-24 17:49:27 -0500

    Integrated routes into OriginalCustomRenderer, and deleted: MapPageRoutes.cs and RouteCumstumRenderer.cs

--- Hermes/Hermes.Android/OriginalCustomRenderer.cs
+++ Hermes/Hermes.Android/OriginalCustomRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 
 using Android.Content;
 using Android.Gms.Maps;
@@ -10,7 +11,8 @@ using Hermes.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Maps.Android;
-using Button = Xamarin.Forms.Button;
+using Newtonsoft.Json;
+using Plugin.Geolocator;
 
 [assembly: ExportRenderer(typeof(OriginalCustomMap), typeof(OriginalCustomRenderer))]
 
@@ -49,6 +51,10 @@ namespace Hermes.Droid
             NativeMap.InfoWindowClick += OnInfoWindowClick;
             NativeMap.SetInfoWindowAdapter(this);
 
+            //Below is the logic for tapping on the screen to add pins directly at that location
+            //This has been replaced with a different way to add pins, so this is being removed 
+            //Also applies to the GoogleMap_MapClick() method below
+            //
             //if (map != null)
             //{
             //    map.MapClick += GoogleMap_MapClick;
@@ -136,6 +142,10 @@ namespace Hermes.Droid
 
         void OnInfoWindowClick(object sender, GoogleMap.InfoWindowClickEventArgs e)
         {
+            //All of this is to have the info window clickable open the URL that is displayed
+            //This has been replaced with logic for a route button that will take precident if
+            //      there is no way to add them both to the info window, which there might not be
+            /*
             var customPin = GetCustomPin(e.Marker);
             if (customPin == null)
             {
@@ -149,6 +159,16 @@ namespace Hermes.Droid
                 intent.AddFlags(ActivityFlags.NewTask);
                 Android.App.Application.Context.StartActivity(intent);
             }
+            */
+
+            var customPin = GetCustomPin(e.Marker);
+            if(customPin == null)
+            {
+                DependencyService.Get<IMessage>().LongAlert("Custom pin not found!");
+                throw new Exception("Custom pin not found!");
+            }
+
+            OnButtonClicked(sender, e, customPin.Position);
         }
 
         CustomPin GetCustomPin(Marker annotation)
@@ -163,5 +183,119 @@ namespace Hermes.Droid
             }
             return null;
         }
+
+        List<Position> FnDecodePolylinePoints(string encodedPoints)
+        {
+            if (encodedPoints == null || encodedPoints == "") return null;
+            List<Position> poly = new List<Position>();
+            char[] polylinechars = encodedPoints.ToCharArray();
+            int index = 0;
+            int currentLat = 0;
+            int currentLng = 0;
+            int next5bits;
+            int sum;
+            int shifter;
+            try
+            {
+                while (index < polylinechars.Length)
+                {
+                    // calculate next latitude
+                    sum = 0;
+                    shifter = 0;
+                    do
+                    {
+                        next5bits = (int)polylinechars[index++] - 63;
+                        sum |= (next5bits & 31) << shifter;
+                        shifter += 5;
+                    } while (next5bits >= 32 && index < polylinechars.Length);
+                    if (index >= polylinechars.Length)
+                        break;
+                    currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                    //calculate next longitude
+                    sum = 0;
+                    shifter = 0;
+                    do
+                    {
+                        next5bits = (int)polylinechars[index++] - 63;
+                        sum |= (next5bits & 31) << shifter;
+                        shifter += 5;
+                    } while (next5bits >= 32 && index < polylinechars.Length);
+                    if (index >= polylinechars.Length && next5bits >= 32)
+                        break;
+                    currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                    Position p = new Position(Convert.ToDouble(currentLat) / 100000.0,
+                        Convert.ToDouble(currentLng) / 100000.0);
+                    poly.Add(p);
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.Source != null)
+                    Console.WriteLine("Exception source: {0}", e.Source);
+                throw;
+            }
+            return poly;
+        }
+
+        public void DrawRoute(GoogleMap g, Position currentPosition, Position destination)
+        {
+            var lat1 = currentPosition.Latitude;
+            var long1 = currentPosition.Longitude;
+            var lat2 = destination.Latitude;
+            var long2 = destination.Longitude; 
+
+            //below is the concatinated url to check outputs
+            //url = https://maps.googleapis.com/maps/api/directions/json?origin=42.02332,-93.66791&destination=42.020454,-93.60969&key=AIzaSyAJ80rolI5NRBXgrvwylHrcAQBGhii_1dI
+            var url = "https://maps.googleapis.com/maps/api/directions/json?" +
+                "origin=" + lat1 + "," + long1 +
+                "&destination=" + lat2 + "," + long2 +
+                "&key=" + "AIzaSyAJ80rolI5NRBXgrvwylHrcAQBGhii_1dI";
+
+            var json = "";
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString(url);
+            }
+
+            RootObject myr = JsonConvert.DeserializeObject<RootObject>(json);
+
+            //DependencyService.Get<IMessage>().LongAlert("checking how many GWP in root:" + myr.Routes[0].Legs.Count);
+
+            var latLngPoints = new LatLng[(myr.Routes[0].Legs[0].Steps.Count) * 2];
+            int index = 0;
+            foreach (var step in myr.Routes[0].Legs[0].Steps)
+            {
+                var startingLat = step.Start_location.Lat;
+                var startingLong = step.Start_location.Lng;
+                var endingLat = step.End_location.Lat;
+                var endingLong = step.End_location.Lng;
+
+                latLngPoints[index++] = new LatLng(startingLat, startingLong);
+                latLngPoints[index++] = new LatLng(endingLat, endingLong);
+            }
+
+            var polylineoption = new PolylineOptions();
+            polylineoption.InvokeColor(Android.Graphics.Color.DarkBlue);
+            polylineoption.Geodesic(true);
+            polylineoption.Add(latLngPoints);
+            g.AddPolyline(polylineoption);
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e, Position destination)
+        {
+            var currentLatitude = 0.0;
+            var currentLongitude = 0.0;
+
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(2000));
+
+            currentLongitude = position.Longitude;
+            currentLatitude = position.Latitude;
+            var currentPosition = new Position(currentLatitude, currentLongitude);
+
+            DrawRoute(NativeMap, currentPosition, destination);
+        }   
     }
 }
